814095a20e3fd3cad100a7c00cd6d5c6fcac21d9
fathia hajjaji <49367379+fathiahjj@users.noreply.github.com>
2019-12-23 16:36:08 +0100

    ent

--- src/main/java/tn/rnu/isetch/microservicepaiement/Entities/Paiement.java
+++ src/main/java/tn/rnu/isetch/microservicepaiement/Entities/Paiement.java
@@ -1,5 +1,46 @@
 package tn.rnu.isetch.microservicepaiement.Entities;
 
-public class Paiement {
+import com.sun.istack.NotNull;
 
+import javax.persistence.*;
+
+@Entity
+public class Paiement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Column(unique = true)
+    private Long code;
+    @NotNull
+    private Long idFacture;
+
+    public Paiement(Long code, Long idFacture) {
+        this.code = code;
+        this.idFacture = idFacture;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCode() {
+        return code;
+    }
+
+    public void setCode(Long code) {
+        this.code = code;
+    }
+
+    public Long getIdFacture() {
+        return idFacture;
+    }
+
+    public void setIdFacture(Long idFacture) {
+        this.idFacture = idFacture;
+    }
 }
